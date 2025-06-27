3f18c2f2f357b7b2628a94ad092aa48378104731
CNHM <huangmou999@qq.com>
2025-06-27 21:26:55 +0800

    Update XHMHUB.lua

--- XHMHUB.lua
+++ XHMHUB.lua
@@ -67,6 +67,34 @@ Tab:AddTextbox({
 	end
 })
 
+Tab:AddToggle({
+	Name = "穿墙",
+	Default = false,
+	Callback = function(Value)
+
+		if Value then
+		    Noclip = true
+		    Stepped = game.RunService.Stepped:Connect(function()
+			    if Noclip == true then
+				    for a, b in pairs(game.Workspace:GetChildren()) do
+                        if b.Name == game.Players.LocalPlayer.Name then
+                            for i, v in pairs(game.Workspace[game.Players.LocalPlayer.Name]:GetChildren()) do
+                                if v:IsA("BasePart") then
+                                    v.CanCollide = false
+                                end
+                            end
+                        end
+                    end
+			    else
+				    Stepped:Disconnect()
+			    end
+		    end)
+	    else
+		    Noclip = false
+	    end
+	end
+})
+
 Tab:AddTextbox({
 	Name = "自定义头部大小",
 	Default = "",
